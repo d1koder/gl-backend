c39b7453c04467087867a4ac09a647350c35fad2
Jacin <jacinjiyan@gmail.com>
2021-01-26 22:02:56 +1100

    added a few more routes and modified a bit controller

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,9 +3,16 @@ class PropertiesController < ApplicationController
         @properties = Property.all
         render json: @properties
     end
+    
     def create
         @property = Property.create(property_params)
+        if @property.error.any?
+            render json: @property.errors, status: :unprocessable_entity
+        else 
+            render json: @property, status: 201
+        end
     end
+
     private
     def property_params
         params.permit(:title, :description, :featured_image, :category_id)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  post "/properties", to: "properties#create"
-  get "/properties", to: "properties#index"
+  scope "/" do
+    get "/properties", to: "properties#index"
+    post "/properties", to: "properties#create"
+    get "/properties/:id", to: "properties#show"
+    put "/properties/:id", to: "properties#update"
+    delete "/properties/:id", to: "properties#destroy"
+  end
 end
