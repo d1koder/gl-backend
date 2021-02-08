7b7be2137f2bf494f23b8225ff9a7c0b38f1a629
DK <dcsk.daniel@gmail.com>
2021-02-08 14:53:57 +1100

    fixed login issue

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,6 +73,6 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.permit(:username, :email, :password, :password_confirmation, :admin)
+    params.permit(:user, :username, :email, :password, :password_confirmation, :admin)
   end
 end
