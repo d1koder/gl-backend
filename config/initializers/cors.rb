20a2e1389481ab2babcc9127f668a12226bbb9fe
DK <dcsk.daniel@gmail.com>
2021-02-08 16:16:25 +1100

    Amended cors origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    # origins 'http://localhost:8080', 'http://localhost:3001', '127.0.0.1:5050'
-    origins '*'
+    origins 'http://localhost:8080', 'http://localhost:3001', '127.0.0.1:5050',
+    'https://6020c557b9896f15204c3d7f--festive-bhaskara-29f081.netlify.app/'
 
     resource '*',
       headers: :any,
